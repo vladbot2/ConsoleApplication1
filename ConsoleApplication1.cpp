4498151a3d1db0d35e8852e4424200b7e60ec889
dev <dev@A21PC12>
2024-09-25 17:31:46 +0300

    Add project files.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,127 @@
+﻿using namespace std;
+#include <iostream>
+
+
+
+
+int main()
+{
+	//1
+	//int a, b;
+
+	//cout << "Enter the first number: ";
+	//cin >> a;
+	//cout << "Enter the second number: ";
+	//cin >> b;
+
+	//int* p1 = &a;
+	//int* p2 = &b;
+
+	//int max;
+	//if (*p1 > *p2) {
+	//	max = *p1;
+	//}
+	//else {
+	//	max = *p2;
+	//}
+
+	//cout << "max number: " << max << endl;
+
+	//2
+	/*int number;
+
+	cout << "Enter number: ";
+	cin >> number;
+
+	int* pNumber = &number;
+
+	if (*pNumber > 0) {
+		cout << " positive :" << endl;
+	}
+	else if (*pNumber < 0) {
+		cout << " negative :" << endl;
+	}
+	else {
+		cout << " zero" << endl;
+	}*/
+
+	//3
+	//int a, b;
+
+	//cout << "Enter the first number: ";
+	//cin >> a;
+	//cout << "Enter the second number: ";
+	//cin >> b;
+
+	//int* p1 = &a;
+	//int* p2 = &b;
+
+	//int temp = *p1; 
+	//*p1 = *p2;      
+	//*p2 = temp;     
+
+	//cout << "After swapping:" << endl;
+	//cout << "First number: " << a << endl;
+	//cout << "Second number: " << b << endl;
+
+	//5
+
+	//const int size = 5; 
+	//int arr[size];     
+	//int sum = 0;       
+
+	//cout << "Enter " << size << " integers:" << endl;
+	//for (int i = 0; i < size; ++i) {
+	//	cout <<в ввввввваа"Element " << (i + 1) << ": ";
+	//	cin >> arr[i];
+	//}
+
+	//int* p = arr;
+
+	//for (int i = 0; i < size; ++i) {
+	//	sum += *(p + i); 
+	//}
+
+	//cout << "Sum of the array elements: " << sum << endl;
+
+	//4
+	//int num1, num2, result;
+	//int operation;
+
+	//cout << "Enter the first number: ";
+	//cin >> num1;
+	//cout << "Enter the second number: ";
+	//cin >> num2;
+	//cout << "Enter the operation (+, -, *, /): ";
+	//cin >> operation;
+
+	//int* pNum1 = &num1;
+	//int* pNum2 = &num2;
+	//int* pResult = &result;
+
+	//if (operation == '+') {
+	//	*pResult = *pNum1 + *pNum2;
+	//}
+	//else if (operation == '-') {
+	//	*pResult = *pNum1 - *pNum2;
+	//}
+	//else if (operation == '*') {
+	//	*pResult = *pNum1 * *pNum2;
+	//}
+	//else if (operation == '/') {
+	//	if (*pNum2 != 0) {
+	//		*pResult = *pNum1 / *pNum2;
+	//	}
+	//	else {
+	//		cout << "Division by zero!" << endl;
+	//		return 1;
+	//	}
+	//}
+	//else {
+	//	cout << "Unknown operation!" << endl;
+	//	return 1;
+	//}
+
+	//cout << "Result: " << result << endl;
+
+}
